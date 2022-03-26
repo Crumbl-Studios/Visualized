2be55ecf5ea1ec1ab868f452a7c47a74b8c5a643
bax2004rj <bax2004rj@gmail.com>
2022-03-26 16:10:15 -0400

    Added variables in fileHandler for buttons Renamed "hover" button to fit file naming scheme

--- fileHandler.py
+++ fileHandler.py
@@ -70,6 +70,10 @@ green_sky = pygame.image.load(os.path.join(backgrounds_dir, 'Green.png'))
 purple_sky = pygame.image.load(os.path.join(backgrounds_dir, 'Purple.png'))
 brown_sky = pygame.image.load(os.path.join(backgrounds_dir, 'Brown.png'))
 
+# Button files:
+button_hover = pygame.image.load(os.path.join(ui_dir, 'Button_hover.png'))
+button = pygame.image.load(os.path.join(ui_dir, 'Button_nohover.png'))
+
 # Character files:
 vr_guy_run_1 = pygame.image.load(os.path.join(vr_guy_dir, 'Run1.png'))
 vr_guy_run_2 = pygame.image.load(os.path.join(vr_guy_dir, 'Run2.png'))
@@ -268,6 +272,7 @@ bat_fly_7 = pygame.image.load(os.path.join(bat_dir, 'Flying7.png'))
 bat_files = [bat_fly_1, bat_fly_2, bat_fly_3, bat_fly_4, bat_fly_5, bat_fly_6,
              bat_fly_7]
 
+# Coin animation files
 
 # Create functions so these files are accessible
 def save_data(data):
