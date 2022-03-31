ff1f95fe3dcdc8cf22172e192d70792971d063bf
Eshan Tahir <Eshancraft@outlook.com>
2022-03-31 12:51:08 -0400

    Created image button outline capability
    Took 29 minutes

--- uiHandler.py
+++ uiHandler.py
@@ -36,7 +36,7 @@ def get_text(font, text, rgb="#000000", aa=False):
 
 # Button system
 class Button:
-    def __init__(self, font, rx=150, ry=100, px=0, py=0, outline_width=6, aa=False, button_type="procedural",
+    def __init__(self, font, rx=150, ry=100, px=0, py=0, outline_width=6, image_outline=False, aa=False, button_type="procedural",
                  selected_box_color="#196985", selected_text_color="#b2b2b2", hover_box_color="#2596be",
                  hover_text_color="#ffffff", box_color="#ffffff", text_color="#2596be", outline_color="#000000",
                  text="button", selected_button_image=None, hover_button_image=None, button_image=None,
@@ -56,6 +56,7 @@ class Button:
         self.text_color = text_color
         self.outline_width = outline_width
         self.outline_color = outline_color
+        self.image_outline = image_outline
 
         self.text = text
         self.rendered_text = None
@@ -94,6 +95,29 @@ class Button:
             self.selected_button_image_rect.topleft = (px, py)
             self.hover_button_image_rect.topleft = (px, py)
 
+            self.button_mask = pygame.mask.from_surface(self.button_image)
+            self.selected_button_mask = pygame.mask.from_surface(self.selected_button_image)
+            self.hover_button_mask = pygame.mask.from_surface(self.hover_button_image)
+
+            self.button_outline_points = []
+            self.selected_button_outline_points = []
+            self.hover_button_outline_points = []
+
+            for point in self.button_mask.outline():
+                x = point[0]+self.button_image_rect[0]
+                y = point[1] + self.button_image_rect[1]
+                self.button_outline_points.append((x, y))
+
+            for point in self.selected_button_mask.outline():
+                x = point[0]+self.hover_button_image_rect[0]
+                y = point[1] + self.hover_button_image_rect[1]
+                self.hover_button_outline_points.append((x, y))
+
+            for point in self.hover_button_mask.outline():
+                x = point[0]+self.selected_button_image_rect[0]
+                y = point[1] + self.selected_button_image_rect[1]
+                self.selected_button_outline_points.append((x, y))
+
             self.button_box = self.button_image
             self.button_box_rect = self.button_image_rect
         else:
@@ -129,12 +153,24 @@ class Button:
         elif self.button_type == "image":
             if not self.clicked_down:
                 if not self.hover:
+                    if self.image_outline:
+                        for point in self.button_outline_points:
+                            pygame.draw.circle(screen, self.outline_color, point, self.outline_width)
                     screen.blit(self.button_image, self.button_image_rect)
                 elif self.hover and self.hover_button_image is not None:
+                    if self.image_outline:
+                        for point in self.hover_button_outline_points:
+                            pygame.draw.circle(screen, self.outline_color, point, self.outline_width)
                     screen.blit(self.hover_button_image, self.hover_button_image_rect)
                 else:
+                    if self.image_outline:
+                        for point in self.button_outline_points:
+                            pygame.draw.circle(screen, self.outline_color, point, self.outline_width)
                     screen.blit(self.button_image, self.button_image_rect)
             elif self.clicked_down:
+                if self.image_outline:
+                    for point in self.selected_button_outline_points:
+                        pygame.draw.circle(screen, self.outline_color, point, self.outline_width)
                 screen.blit(self.selected_button_image, self.selected_button_image_rect)
 
     def click_check(self, cursor_rect, events):
